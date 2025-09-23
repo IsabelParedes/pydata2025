f15477bbef3cf54d63037b272169ea3d4ec2db2e
Isabel Paredes <isabel.paredes@quantstack.net>
2025-09-23 19:39:21 +0200

    WIP Flang in action

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import './style.css'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/plugin/highlight/monokai.css'
 import Reveal from 'reveal.js';
 import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm.js';
 import RevealNotes from 'reveal.js/plugin/notes/notes.esm.js';
 import RevealMath from 'reveal.js/plugin/math/math.esm.js';
+
+import './style.css'
 
 Reveal.initialize({
   controls: true,
